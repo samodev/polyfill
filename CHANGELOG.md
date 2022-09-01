ceac92746c078d2ec543a5970b671006c4ba84ba
samomor-dev <112557801+samomor-dev@users.noreply.github.com>
2022-09-01 13:43:22 +0200

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# CHANGELOG
+
+## 0.3.1 Sep 17, 2020
+
+- x-textdecoder & x-textencoder
+
+## 0.2.1 Sep 16, 2020
+
+- x-fetch & x-ws
+
+## 0.1.1 Sep 16, 2020
+
+- Initial release
---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// Copyright 2020 @polkadot/polyfill authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const config = require('@polkadot/dev/config/jest');
+
+module.exports = Object.assign({}, config, {
+  moduleNameMapper: {
+    '@polkadot/(x-fetch|x-ws)(.*)$': '<rootDir>/packages/$1/src/$2'
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/packages/*/build'
+  ],
+  resolver: '@polkadot/dev/config/jest-resolver'
+});
